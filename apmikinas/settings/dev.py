4f6f871a86e82f720e5964df11bc8b338aa53832
yuto-dev <directx178@gmail.com>
2020-01-31 06:49:10 +0000

    first commit

---
+++ apmikinas/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'q*)00r_6*r$!6)b%7xifw^22nu7euefvoyb6rdr(av!uw2*_6-'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
